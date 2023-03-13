d21a59b794a5161e62188d0e3d9af7b968047886
Shymala Sidharth <shymala.sidharth22@gmail.com>
2023-03-14 11:19:38 +1300

    starting on game

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+use std::io;
+
+
+use std:: io;
+
 fn main() {
     println!("Guess the number!");
 
-    print!("Please input your guess.")
+    print!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    print!("You guessed: {guess}")
+
+    
 }
