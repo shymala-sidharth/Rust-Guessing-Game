3e2a8f64c1a49ece0020e3bef1da496d422288ed
Shymala Sidharth <shymala.sidharth22@gmail.com>
2023-03-15 09:27:28 +1300

    adding comments to expain code

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,12 @@
-use std::io;
-
-
-use std:: io;
+use std::io; //use of standard library std. Use the io library to obtain user input and print the result as output
 
 fn main() {
     println!("Guess the number!");
 
     print!("Please input your guess.");
 
-    let mut guess = String::new();
+    let mut guess = String::new(); //introduce a mutable variable called 'guess' and binds String::new to it using = String::new is a function that returns a new instance of a string. String is a string type provided by the standard library.
+    // created a mutable variable that is currently bound to a new, empty instance of a string.
 
     io::stdin().read_line(&mut guess).expect("Failed to read line");
 
